6310a997db3d9953f2ec1741aa947d3cea3dfa55
Magd El Zalameh <Magd El Zalameh>
2025-08-09 17:50:33 +0300

    Auth Pages

--- lib/appwrite.tsx
+++ lib/appwrite.tsx
@@ -2,5 +2,6 @@ export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     platform: 'com.magd.foodix',
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-
+    databaseId: '6897595600074d164553',
+    userCollectionId: '689759db00269151612d',
 }
